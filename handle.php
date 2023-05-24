c66793a341ea5ea313bec32aa83c8b14975ee261
Sadiq <113123466+sadiqalhumood@users.noreply.github.com>
2023-05-24 01:14:14 -0400

    added handle error file to see errors and edited

--- handle.php
+++ handle.php
@@ -1,10 +1,9 @@
 <?php
-    $name= $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $visitor_email = isset($_POST['email']) ? $_POST['email'] : '';
+    $message = isset($_POST['message']) ? $_POST['message'] : '';
 
-
-    $email_from='sadiq.humood@gmail.com';
+    $email_from = 'sadiq.humood@gmail.com';
 
     $email_subject = "New Form Submission";
 
@@ -12,14 +11,15 @@
                         "User Email: $visitor_email.\n".
                             "User Message: $message.\n";
 
-$to = "sadiq.humood@gmail.com";
+    $to = "sadiq.humood@gmail.com";
 
-$headers = "From: $email_from \r\n";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8 \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: index.html");
-
+    if(mail($to,$email_subject,$email_body,$headers)){
+        header("Location: contact.html");
+    } else{
+        error_log("Email failed to send. Check your PHP mail configuration.", 3, "mail_error_log.txt");
+    }
 ?>
